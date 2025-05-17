a9b8cf9bac10e6c4cc386cd9db6da648568c950e
danielselaru <selarud@yahoo.com>
2025-05-18 01:26:35 +0300

    asd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Header from '../header/Header'
 import Api_link from '../api/Api_link'
 
+
 function Home() {
   return (
     <div>
       <Header></Header>
       <Api_link></Api_link>
-    </div>
-  )
-}
+      </div>
+)}
 
 export default Home
